311b23239adfdfaa03200530c32f08edee39fea7
Markus Kainu <markuskainu@gmail.com>
2014-06-16 16:10:57 +0300

    questionnaire updated

--- compile.sh
+++ compile.sh
@@ -9,7 +9,7 @@ pandoc -s -S --toc --number-section -H rmarkdown_survey.css form.md -o form.html
 pandoc --toc --number-section form.md -o form.docx
 pandoc --toc --number-section form.md -o form.odt
 #pandoc --toc --number-section --latex-engine=xelatex -V lang=english -V papersize:a4paper -V documentclass=scrartcl form.md -o form.pdf
-pandoc -s -S -H /home/aurelius/workspace/web/css/rmarkdown.css index.md -o index.html
+pandoc -s -S -H /home/aurelius/web/css/rmarkdown.css index.md -o index.html
 
 git commit -am "questionnaire updated"
 git push
